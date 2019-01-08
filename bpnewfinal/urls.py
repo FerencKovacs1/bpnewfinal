63a139633dc21903c7a514ee03749519b7c1a04e
fkovacs12 <yepense@gmail.com>
2019-01-08 12:16:06 +0100

    With brand logos and footer

--- bpnewfinal/urls.py
+++ bpnewfinal/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^$',views.homepage),
     url(r'', include('core.urls')),
     url(r'^szerviz/',include('core.urls')),
 
